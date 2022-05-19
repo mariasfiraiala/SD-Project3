0e0783500eed79e39dc1e5e63348a2765d2ee762
mariasfiraiala <maria.sfiraiala@gmail.com>
2022-05-19 16:53:24 +0300

    Add solution for createFileTree
    Signed-off-by: mariasfiraiala <maria.sfiraiala@gmail.com>

--- tree.c
+++ tree.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "tree.h"
 #define TREE_CMD_INDENT_SIZE 4
 #define NO_ARG ""
 #define PARENT_DIR ".."
 
 
-FileTree createFileTree(char* rootFolderName) {
-    // TODO
+FileTree *createFileTree(char* rootFolderName)
+{
+    FileTree *new = (FileTree *)malloc(sizeof(*new));
+    DIE(!new, "malloc() failed\n");
+
+    new->root->content = strdup(rootFolderName);
+    new->root->content = new->root->parent = NULL;
+    new->root->type = FOLDER_NODE;
+
+    return new;
 }
 
-void freeTree(FileTree fileTree) {
-    // TODO
+void freeTree(FileTree **fileTree)
+{
+
 }
 
 
